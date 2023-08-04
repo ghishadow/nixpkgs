5e1a942825ab8392c282a7ad2d3ca1bdf0d8969d
Suraj Ghimire <suraj@ghishadow.com>
2023-08-05 02:00:17 +0530

    update drm_info
    Signed-off-by: Suraj Ghimire <suraj@ghishadow.com>

--- pkgs/development/tools/drm_info/default.nix
+++ pkgs/development/tools/drm_info/default.nix
@@ -1,25 +1,26 @@
-{ lib, stdenv, fetchFromGitHub
+{ lib, stdenv, fetchFromGitLab
 , libdrm, json_c, pciutils
-, meson, ninja, pkg-config
+, meson, ninja, pkg-config, cmake, scdoc
 }:
 
 stdenv.mkDerivation rec {
   pname = "drm_info";
-  version = "2.3.0";
+  version = "2.5.0";
 
-  src = fetchFromGitHub {
-    owner = "ascent12";
+  src = fetchFromGitLab {
+    domain = "gitlab.freedesktop.org";
+    owner = "emersion";
     repo = "drm_info";
     rev = "v${version}";
-    sha256 = "sha256-UTDYLe3QezPCyG9CIp+O+KX716JDTL9mn+OEjjyTwlg=";
+    sha256 = "sha256-gNuBN4m7xdK7oGy1VD8zOGVPZFUaQDKSlLHI6yC6zp4=";
   };
 
-  nativeBuildInputs = [ meson ninja pkg-config ];
+  nativeBuildInputs = [ meson ninja pkg-config scdoc cmake ];
   buildInputs = [ libdrm json_c pciutils ];
 
   meta = with lib; {
     description = "Small utility to dump info about DRM devices";
-    homepage = "https://github.com/ascent12/drm_info";
+    homepage = "https://gitlab.freedesktop.org/emersion/drm_info";
     license = licenses.mit;
     maintainers = with maintainers; [ tadeokondrak ];
     platforms = platforms.linux;
